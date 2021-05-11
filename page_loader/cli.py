869ab1927c998b5148fa74618885969bf927bacc
Vladislav Sviridov <v.sviridov.dev@gmail.com>
2021-05-11 23:00:42 +0300

    Fix positional arguments

--- page_loader/cli.py
+++ page_loader/cli.py
@@ -6,10 +6,10 @@ def get_parser():
     parser = argparse.ArgumentParser(description='Page saving')
     parser.add_argument(
         '-o', '--output',
+        type=str,
         help='output direcotry',
-        default=os.getcwd()
+        default=os.getcwd(),
     )
-    parser.add_argument('path')
     parser.add_argument('url')
 
     return parser
@@ -18,9 +18,8 @@ def get_parser():
 def parse_args():
     parser = get_parser()
     args = parser.parse_args()
-    if os.path.isabs(args.path):
-        path = os.path.join(os.getcwd(), args.path)
+    if os.path.isabs(args.output):
+        path = os.path.join(os.getcwd(), args.output)
     else:
-        path = args.path
-
+        path = args.output
     return (path, args.url)
